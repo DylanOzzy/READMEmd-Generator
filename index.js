dadb7f28f4bfbb68e613e096fd26687b0c102e91
unknown <dylanosborn123@hotmail.com>
2023-11-19 21:43:30 -0600

    Added javascript that enables proper license documentation and badge to be displayed based on user input. Created sample Read me.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const fs = require("fs").promises;
 const path = require("path");
 const { error } = require("console");
 const userData = {};
+// In Progess
+let {
+  renderLicenseSection,
+} = require("./utils/generateMarkdown");
 
 const promptUser = async () => {
   return inquirer
@@ -11,53 +15,59 @@ const promptUser = async () => {
       {
         type: "input",
         name: "projectName",
-        message: "What is the name of your project?",
+        message: "What is the name of your project?"
       },
       {
         type: "input",
         name: "description",
-        message: "How would you describe your project? (Enter N/A if not required)",
+        message:
+          "How would you describe your project? (Enter N/A if not required)"
       },
       {
         type: "input",
         name: "installation",
-        message: "How can your project be installed? (Enter N/A if not required)",
+        message:
+          "How can your project be installed? (Enter N/A if not required)"
       },
       {
         type: "input",
         name: "usage",
-        message: "How does a user navigate the application? (Enter N/A if not required)",
+        message:
+          "How does a user navigate the application? (Enter N/A if not required)"
       },
       {
         type: "list",
         name: "license",
-        message: "Which license would you like to use?",
+        message: "Which license would you like to use? (Enter N/A if not required)",
         choices: [
-          "GNU AGPLv3",
-          "GNU GPLv3",
-          "GNU LGPLv3",
-          "Mozilla Public License 2.0",
-          "Apache License 2.0",
-          "MIT License",
-          "Boost Software License 1.0",
-          "The Unlicense",
-          "No License",
+          { title: 'MIT License', value: 'MIT' },
+          { title: 'Mozilla Public License 2.0', value: 'MPL2.0' },
+          { title: 'The Unlicense', value: 'Unlicense' }
         ],
       },
       {
         type: "input",
         name: "contribution",
-        message: "How can other developers contribute to the project? (Enter N/A if not required)",
+        message:
+          "How can other developers contribute to the project? (Enter N/A if not required)"
       },
       {
         type: "input",
         name: "tests",
-        message: "What are your instructions for testing the application? (Enter N/A if not required)",
+        message:
+          "What are your instructions for testing the application? (Enter N/A if not required)"
       },
       {
         type: "input",
-        name: "contact",
-        message: "How can contributors contact you about the project? (Enter N/A if not required)",
+        name: "github",
+        message:
+          "Enter your GitHub username so contributors can contact you about the project."
+      },
+      {
+        type: "input",
+        name: "email",
+        message:
+          "Enter your E-Mail so contributors can contact you about the project."
       },
     ])
     .then((data) => {
@@ -65,20 +75,22 @@ const promptUser = async () => {
       return userData;
     });
 };
-
+// In Progess
 const generateMarkdown = async (userData) => {
+  const licenseSection = renderLicenseSection(userData.license);
   const readmeContent = `# ${userData.projectName}
-
+  [![License: ${userData.license}](https://img.shields.io/badge/License-${userData.license}-yellow.svg)](https://opensource.org/licenses/${userData.license}) 
+    
   ## Table of Contents
   
-  - [Project Description](#project-Description)
-  - [Installation](#Installation)
-  - [Usage](#Usage)
-  - [License](#License)
-  - [Contribution](#Contribution)
-  - [Tests](#Tests)
-  - [Contact](#Contact)
-  
+  - [Project Description](#project-description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [Contact For Questions](#contact-for-questions)
+  - [License](#license)
+
   
   ## Project Description
   
@@ -92,9 +104,6 @@ const generateMarkdown = async (userData) => {
   
   ${userData.usage}
   
-  ## License
-  
-  ${userData.license} 
   
   ## Contribution
   
@@ -104,15 +113,25 @@ const generateMarkdown = async (userData) => {
   
   ${userData.tests}
   
-  ## Contact
+  ## Contact For Questions
   
-  ${userData.contact}`;
+  Please reach out to me for any inqires about this project through a GitHub message or an E-Mail!
+  - Github: [${userData.github}](https://github.com/${userData.github}/)
+  - E-Mail: <${userData.email}>
+
+  ## License
+  
+  ${licenseSection}`;
+
   return readmeContent;
 };
 
 const handleSaveJson = async () => {
   const saveJson = "./data-storage";
-  const fileName = `${userData.projectName.toLowerCase().split(" ").join("")}.json`;
+  const fileName = `${userData.projectName
+    .toLowerCase()
+    .split(" ")
+    .join("")}.json`;
   const jsonPath = path.join(__dirname, saveJson, `${fileName}`);
   fs.writeFile(jsonPath, JSON.stringify(userData, null, "\t"));
 };
---
+++ new-readme/Baby Bunny Bumper Boats.md
@@ -0,0 +1,67 @@
+# Baby Bunny Bumper Boats
+  [![License: Unlicense](https://img.shields.io/badge/License-Unlicense-yellow.svg)](https://opensource.org/licenses/Unlicense) 
+    
+  ## Table of Contents
+  
+  - [Project Description](#project-description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [Contact For Questions](#contact-for-questions)
+  - [License](#license)
+
+  
+  ## Project Description
+  
+  Baby Bunny Bumper Boats offer whimsical aquatic adventures for little ones. These adorable, safe boats provide a joyful experience, as young ones navigate gentle waters with playful bunnies, creating precious memories.
+  
+  ## Installation
+  
+  Navigate to the BBBB Repo and clone it to your local machine.
+  
+  ## Usage
+  
+  Open Google. Navigate to GitHub.com/BBBB.
+  
+  
+  ## Contribution
+  
+  For the repo from the BBBB Git Hub repository linked below.
+  
+  ## Tests
+  
+  For the repo from the BBBB Git Hub repository linked below.
+  
+  ## Contact For Questions
+  
+  Please reach out to me for any inqires about this project through a GitHub message or an E-Mail!
+  - Github: [BigBunny](https://github.com/BigBunny/)
+  - E-Mail: <BigBunny@bunny.com>
+
+  ## License
+  
+  This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+    
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+    For more information, please refer to <https://unlicense.org>
